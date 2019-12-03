6262e5e17041729d39bc897d0e38c380dde68da7
dicorndl <dicorndl@gmail.com>
2019-12-03 16:43:20 +0900

    8. input 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Hello from "./Hello";
 import './App.css';
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
+import InputSample from "./InputSample";
 
 function App() {
   return (
-      <Wrapper>
-        <Hello name="react" color="red" isSpecial/>
-        <Hello color="pink"/>
-        <Counter/>
-      </Wrapper>
+      <div>
+        <Wrapper>
+          <Hello name="react" color="red" isSpecial/>
+          <Hello color="pink"/>
+          <Counter/>
+        </Wrapper>
+        <InputSample/>
+      </div>
   );
 }
 
