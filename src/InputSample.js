41dcf7506323b1da187fe474abe8fdb7ad941d69
dicorndl <dicorndl@gmail.com>
2019-12-04 16:31:56 +0900

    10. useRef 로 특정 DOM 선택하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: '',
     nickname: ''
   });
+  const nameInput = useRef();
 
-  const {name, nickname} = inputs;
+  const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    const {value, name} = e.target;
+    const { value, name } = e.target;
     setInputs({
       ...inputs,
       [name]: value
@@ -20,19 +21,31 @@ function InputSample() {
     setInputs({
       name: '',
       nickname: ''
-    })
+    });
+    nameInput.current.focus();
   };
 
   return (
+    <div>
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <button onClick={onReset}>초기화</button>
       <div>
-        <input name="name" placeholder="이름" onChange={onChange} value={name}/>
-        <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
-        <button onClick={onReset}>초기화</button>
-        <div>
-          <b>값: </b>
-          {name} ({nickname})
-        </div>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
+    </div>
   );
 }
 
