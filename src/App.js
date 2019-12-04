a2de38f758a0ace6c2231e4c1984876282c0e196
dicorndl <dicorndl@gmail.com>
2019-12-04 16:54:33 +0900

    12. useRef 로 컴포넌트 안의 변수 만들기13. 배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from "./Hello";
 import './App.css';
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -23,10 +24,34 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
 
@@ -38,6 +63,12 @@ function App() {
         <Counter />
       </Wrapper>
       <InputSample />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </div>
   );
